3aad12c24fe4fa9844a00b2518b133dccc411478
Martin <martin.n.sarubbi@hotmail.com>
2022-12-08 18:13:17 -0300

    Last changes

--- src/services/UploadFile.js
+++ src/services/UploadFile.js
@@ -1,13 +1,13 @@
 
 const axios = require('axios').default;
-const BACKEND_URL = 'http://opticar-env.eba-arzsmdu7.us-east-1.elasticbeanstalk.com:80/api'
+const BACKEND_URL = 'https://cors-anywhere.herokuapp.com/http://opticar-env.eba-arzsmdu7.us-east-1.elasticbeanstalk.com'
   
 export async function uploadFile(bulkData) {
 
   const postInfo = bulkData;
 
   console.log(postInfo);
-  const response = await axios.post(BACKEND_URL + '/bulk-upload', bulkData)
+  const response = await axios.post(BACKEND_URL + '/api/bulk-upload', bulkData)
   console.log("121212")
   console.log(response);
 
